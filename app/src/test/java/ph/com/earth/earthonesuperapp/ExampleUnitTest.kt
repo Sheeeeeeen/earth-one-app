9a0245c46c6461ce277dd6262d6e32691e4fa4ab
Sheen Fabile <sheen.fabile@globe.com.ph>
2024-02-05 08:15:39 +0800

    add ktlint for code formatter and linter

--- app/src/test/java/ph/com/earth/earthonesuperapp/ExampleUnitTest.kt
+++ app/src/test/java/ph/com/earth/earthonesuperapp/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package ph.com.earth.earthonesuperapp
 
-import org.junit.Test
-
 import org.junit.Assert.*
+import org.junit.Test
 
 /**
  * Example local unit test, which will execute on the development machine (host).
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,9 @@
 plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.kotlinAndroid) apply false
+    id("org.jlleitschuh.gradle.ktlint") version "11.6.1" apply false
+}
+
+allprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
 }
