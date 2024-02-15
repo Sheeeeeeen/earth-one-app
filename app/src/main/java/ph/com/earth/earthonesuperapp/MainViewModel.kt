c402bd5bee03a5d1b43331bf2e498dd744ccaf40
Sheen Fabile <sheen.fabile@globe.com.ph>
2024-02-15 15:23:14 +0800

    working MainViewModelTest.kt

--- app/src/main/java/ph/com/earth/earthonesuperapp/MainViewModel.kt
+++ app/src/main/java/ph/com/earth/earthonesuperapp/MainViewModel.kt
@@ -3,6 +3,7 @@ package ph.com.earth.earthonesuperapp
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
@@ -16,7 +17,7 @@ class MainViewModel @Inject constructor(private val userRepository: UserReposito
     private val _uiState: MutableStateFlow<MainUiState> = MutableStateFlow(MainUiState())
     val uiState = _uiState.asStateFlow()
 
-    fun observeUserLoggedInStatus() {
+    init {
         viewModelScope.launch {
             userRepository.isUserLoggedIn.collect { status ->
                 _uiState.update { it.copy(isLoading = status) }
--- app/src/test/java/ph/com/earth/earthonesuperapp/MainViewModelTest.kt
+++ app/src/test/java/ph/com/earth/earthonesuperapp/MainViewModelTest.kt
@@ -5,57 +5,74 @@ import io.mockk.coEvery
 import io.mockk.mockk
 import io.mockk.verify
 import junit.framework.TestCase.assertEquals
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.test.TestDispatcher
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.advanceTimeBy
+import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.runCurrent
-import org.junit.Rule
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import org.junit.After
+import org.junit.Before
 import org.junit.Test
 import ph.com.earth.data.user.UserRepository
 
 @OptIn(ExperimentalCoroutinesApi::class)
 class MainViewModelTest {
 
-    @get:Rule
-    val testCoroutineRule = TestCoroutineRule()
+//    @get:Rule
+//    val testCoroutineRule = TestCoroutineRule()
+
+    private val testCoroutineDispatcher: TestDispatcher = UnconfinedTestDispatcher()
 
     private val userRepository: UserRepository = mockk()
 
-    private val mainViewModel: MainViewModel = MainViewModel(userRepository)
+    private lateinit var mainViewModel: MainViewModel
+
+    @Before
+    fun setup() {
+        Dispatchers.setMain(testCoroutineDispatcher)
+    }
+
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain()
+    }
 
     @Test
-    fun `test MainUiState with default value of isLoading = true `() = testCoroutineRule.runTest {
-
+    fun `test MainUiState with default value of isLoading = true `() = runTest {
+        //Arrange
         val userLoggedInFlow = MutableStateFlow(true)
-
         coEvery { userRepository.isUserLoggedIn } returns userLoggedInFlow
 
-        mainViewModel.observeUserLoggedInStatus()
+        //Action
+        mainViewModel = MainViewModel(userRepository)
 
-        runCurrent()
-
+        //Assert
         mainViewModel.uiState.test {
             val expectedValue = MainUiState(isLoading = true)
             assertEquals(expectedValue, awaitItem())
         }
-
         verify { userRepository.isUserLoggedIn }
     }
 
     @Test
-    fun `test MainUiState with default value of isLoading = false `() = testCoroutineRule.runTest {
+    fun `test MainUiState with default value of isLoading = false `() = runTest {
+        //Arrange
         val userLoggedInFlow = MutableStateFlow(false)
-
         coEvery { userRepository.isUserLoggedIn } returns userLoggedInFlow
 
-        mainViewModel.observeUserLoggedInStatus()
+        //Action
+        mainViewModel = MainViewModel(userRepository)
 
-        runCurrent()
-
+        //Assert
         mainViewModel.uiState.test {
             val expectedValue = MainUiState(isLoading = false)
             assertEquals(expectedValue, awaitItem())
         }
-
         verify { userRepository.isUserLoggedIn }
     }
 }
